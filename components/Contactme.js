f396f145f1e1fbb12dec6def8edc961d2f4c36d8
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-30 19:57:27 +0530

    Update Contactme.js

--- components/Contactme.js
+++ components/Contactme.js
@@ -3,115 +3,64 @@ import React from "react";
 const Contactme = () => {
   return (
     <>
-      <div name="contact" className='max-w-[1240px] m-auto pt-8 mt-8 pb-5'>
-        <form  className='max-w-[500px] m-auto pt-10 pl-2'>
-          <div class="relative z-0 mb-6 w-full group">
-            <input
-              type="email"
-              name="floating_email"
-              id="floating_email"
-              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
-            />
-            <label
-              for="floating_email"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Email address
-            </label>
+      <div name="contact" className="w-full mt-5 pt-5">
+        <div className="max-w-[1240px] mx-auto pt-3">
+          <div className="text-center">
+            <h2 className="text-[#8109f8] italic text-6xl font-bold">
+              Contact Me
+            </h2>
           </div>
-          
-          
-          <div class="grid md:grid-cols-2 md:gap-6">
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="text"
-                name="floating_first_name"
-                id="floating_first_name"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
+
+          <div className="grid md:grid-cols-4 gap-4 pl-2 mt-8 pt-8 pb-20 text-center">
+            <div className="border py-8 rounded-xl hover:scale-105 duration-300">
+              <p className="text-black  text-2xl font-bold mt-2">LinkedIn</p>
+              <a href="https://www.linkedin.com/in/shahbaz-sheikh-b1477b135/">
+                <p className="mt-10 px-5 pr-5 italic text-xl">linkedin.com</p>
+              </a>
+              <img
+                className="w-60 mx-auto mt-10 bg-white"
+                src="linkedin.jpg"
+                alt="/"
               />
-              <label
-                for="floating_first_name"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                First name
-              </label>
             </div>
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="text"
-                name="floating_last_name"
-                id="floating_last_name"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
+            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
+              <p className="text-black text-2xl font-bold mt-2">Email </p>
+              <p className="mt-10 px-2 italic text-xl">
+                shahbazsheikh9175@gmail.com
+              </p>
+              <img
+                className="w-60 mx-auto mt-10 bg-white"
+                src="email.jpg"
+                alt="/"
               />
-              <label
-                for="floating_last_name"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Last name
-              </label>
+            </div>
+
+            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
+              <p className="text-black  text-2xl font-bold mt-2">Github</p>
+              <a href="https://github.com/"><p className="mt-10 px-2 italic text-xl">
+                Shahbazsheikh7888
+              </p></a>
+              <img
+                className="w-60 mx-auto mt-10 bg-white"
+                src="github.webp"
+                alt="/"
+              />
+            </div>
+            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
+              <p className="text-black  text-2xl font-bold mt-2">Website</p>
+              <a href="https://skshahbaz.netlify.app/"><p className="mt-10 px-2 italic text-xl">
+              https://skshahbaz.netlify.app/
+              </p></a>
+              <img
+                className="w-60 mx-auto mt-10 bg-white"
+                src="website.jpg"
+                alt="/"
+              />
             </div>
           </div>
-          <div class="grid md:grid-cols-2 md:gap-6">
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="tel"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                name="floating_phone"
-                id="floating_phone"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
-              />
-              <label
-                for="floating_phone"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Phone number
-              </label>
-            </div>
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="text"
-                name="floating_company"
-                id="floating_company"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
-              />
-              <label
-                for="floating_company"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Company name
-              </label>
-            </div>
-            
-          </div>
-          <textarea className='border shadow-lg p-3 w-full' cols="30" rows="8" placeholder='Message'></textarea>
-          <div className="flex justify-center mt-5"> <button className=' text-2xl border rounded-full  bg-purple-600 hover:bg-purple-800 shadow-lg p-2 text-white w-40  mt-2'>Submit</button></div>
-         
-        </form>
+        </div>
       </div>
-      {/* <div className='max-w-[1240px] m-auto pt-2 pb-5'>
-        
-        <form className='max-w-[600px] m-auto '>
-            <div className='grid grid-cols-2 gap-2'>
-                <input className='border shadow-lg p-3' type="text" placeholder='Name' />
-                
-                <input className='border shadow-lg p-3' type="email" placeholder='Email' />
-               
-            </div>
-            <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Business Name' />
-            <textarea className='border shadow-lg p-3 w-full' cols="30" rows="8" placeholder='Message'></textarea>
-            <button className=' text-2xl border bg-purple-600 hover:bg-purple-800 shadow-lg p-2 text-white w-full mt-2'>Submit</button>
-        </form>
-    </div> */}
+
       <hr className="h-1 mx-20 mt-2 animate-pulse bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  " />
     </>
   );
