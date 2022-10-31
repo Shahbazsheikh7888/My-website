6e7e4c8821f5f46e2dbbd3e3f662b8241f83b821
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-31 09:17:49 +0530

    Update Contactme.js

--- components/Contactme.js
+++ components/Contactme.js
@@ -6,16 +6,16 @@ const Contactme = () => {
       <div name="contact" className="w-full mt-5 pt-5">
         <div className="max-w-[1240px] mx-auto pt-3">
           <div className="text-center">
-            <h2 className="text-[#8109f8] italic text-6xl font-bold">
+            <h2 className="text-[#f19217] italic text-6xl font-bold">
               Contact Me
             </h2>
           </div>
 
           <div className="grid md:grid-cols-4 gap-4 pl-2 mt-8 pt-8 pb-20 text-center">
-            <div className="border py-8 rounded-xl hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl hover:bg-orange-100 hover:scale-105 duration-300">
               <p className="text-black  text-2xl font-bold mt-2">LinkedIn</p>
               <a href="https://www.linkedin.com/in/shahbaz-sheikh-b1477b135/">
-                <p className="mt-10 px-5 pr-5 italic text-xl">linkedin.com</p>
+                <p className="mt-10 px-5 pr-5 hover:text-orange-500 italic text-xl">linkedin.com</p>
               </a>
               {/* <img
                 className="w-60 mx-auto mt-10 bg-white"
@@ -23,7 +23,7 @@ const Contactme = () => {
                 alt="/"
               /> */}
             </div>
-            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl hover:bg-orange-100 hover:scale-105 duration-300">
               <p className="text-black text-2xl font-bold mt-2">Email </p>
               <p className="mt-10 px-2 italic text-xl">
                 shahbazsheikh9175@gmail.com
@@ -35,9 +35,9 @@ const Contactme = () => {
               /> */}
             </div>
 
-            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl hover:bg-orange-100  hover:scale-105 duration-300">
               <p className="text-black  text-2xl font-bold mt-2">Github</p>
-              <a href="https://github.com/"><p className="mt-10 px-2 italic text-xl">
+              <a href="https://github.com/"><p className="mt-10 px-2 italic text-xl hover:text-orange-500">
                 Shahbazsheikh7888
               </p></a>
               {/* <img
@@ -46,9 +46,9 @@ const Contactme = () => {
                 alt="/"
               /> */}
             </div>
-            <div className="border py-8 rounded-xl  hover:scale-105 duration-300">
+            <div className="border py-8 rounded-xl hover:bg-orange-100 hover:scale-105 duration-300">
               <p className="text-black  text-2xl font-bold mt-2">Website</p>
-              <a href="https://skshahbaz.netlify.app/"><p className="mt-10 px-2 italic text-xl">
+              <a href="https://skshahbaz.netlify.app/"><p className="mt-10 px-2 hover:text-orange-500 italic text-xl">
               https://skshahbaz.netlify.app/
               </p></a>
               {/* <img
@@ -61,7 +61,7 @@ const Contactme = () => {
         </div>
       </div>
 
-      <hr className="h-1 mx-20 mt-2 animate-pulse bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  " />
+      <hr className="h-1 mx-20 animate-pulse bg-gray-100 "/>
     </>
   );
 };
