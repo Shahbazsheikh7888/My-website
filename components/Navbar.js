e293c124d8ae8d8a36ff920f29d9e4604c8466de
Shahbazsheikh7888 <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-10-31 09:19:45 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,32 +11,32 @@ const Navbar = () => {
   const handleClose = () => setNav(!nav);
 
   return (
-    <div className="w-screen h-[80px] z-10 bg-gradient-to-r from-purple-100 via-purple-200 to bg-purple-100 fixed drop-shadow-lg">
+    <div className="w-screen h-[80px]  bg-white fixed pt-3">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl ml-7 font-bold italic mr-10 sm:text-4xl">Welcome !</h1>
-          <ul className="hidden text-black md:flex ml-40 pl-40   mx-10 text-xl p-5 md:space-x-8">
-            <li>
+          <h1 className="text-3xl ml-7 font-bold italic mr-10 text-[#f19217] sm:text-4xl">Welcome !</h1>
+          <ul className="hidden text-black md:flex ml-40 pl-40 text:hover-yellow  mx-10 text-xl p-5 md:space-x-8">
+            <li className=" hover:underline hover:text-orange-500">
               <Link to="home" smooth={true} duration={500} class="cursor-pointer">
                 Home
               </Link>
             </li>
-            <li>
+            <li className=" hover:underline hover:text-orange-500 ">
               <Link to="aboutme" smooth={true} offset={-200} duration={500} class="cursor-pointer">
                 About me
               </Link>
             </li>
-            <li>
+            <li className=" hover:underline hover:text-orange-500">
               <Link to="myskills" smooth={true} offset={-50} duration={500} class="cursor-pointer">
                 My skills
               </Link>
             </li>
-            <li>
+            <li className=" hover:underline hover:text-orange-500">
               <Link to="projects" smooth={true} offset={-100} duration={500} class="cursor-pointer">
                 Projects
               </Link>
             </li>
-            <li>
+            <li className=" hover:underline hover:text-orange-500">
               <Link to="resume" smooth={true} offset={-50} duration={500} class="cursor-pointer">
                 Resume
               </Link>
@@ -45,7 +45,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
-          <Link to="contact"><button  className="text-white bg-purple-600 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full mx-5 text-xl italic px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+          <Link to="contact"><button  className="text-white bg-[#f19217] hover:bg-black focus:outline-none focus:ring-4  font-medium rounded-full mx-5 text-xl italic px-5 py-2.5 text-center mb-2 ">
             Contact Me
           </button></Link>
           
@@ -113,9 +113,9 @@ const Navbar = () => {
         
 
         <div className="flex flex-col my-4">
-        <a href="https://marketplace.flexabledats.com/"><button className="bg-transparent text-black hover:text-purple-800 px-8 ">
+        <Link to="contact"><button  className="text-white bg-[#f19217] hover:bg-black focus:outline-none focus:ring-4  font-medium rounded-full mx-5 text-xl w-30 italic px-5 py-2.5 text-center mb-2 ">
             Contact Me
-          </button></a>
+          </button></Link>
           
         </div>
       </ul>
